17088ed41839065dd51922ff539c8a7daf46284c
Katy-kenny <e.i.volskaia@gmail.com>
2024-08-25 20:16:20 +0300

    module_1_3.py

--- module_1_3.py
+++ module_1_3.py
@@ -2,7 +2,7 @@ name = 'Kate'
 print('Name:', name)
 age = 32
 print('Age:', age)
-new_age = 33
-print('New age:', new_age)
+age = 33
+print('New age:', age)
 is_student = 'True'
 print('Is Student:', is_student)
